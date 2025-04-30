44ec67ffadf80e55569808b2b3d1acecd44a7dac
Pedro Collares <Pedro.Collares.Xavier@gmail.com>
2025-04-30 14:25:26 -0300

    Criação dos modelos do Banco de Dados

--- ToDoList/ToDoList/settings.py
+++ ToDoList/ToDoList/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #padrão do django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #do projeto ToDoList
+    'tasks',
+    'users',
 ]
 
 MIDDLEWARE = [
--- ToDoList/tasks/models.py
+++ ToDoList/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User  #usuário padrão do django
 
-# Create your models here.
+class Task(models.Model):
+    titulo = models.CharField(max_length=255)
+    descricao = models.TextField()
+    completo = models.BooleanField(default=False)
+    criado_em = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo

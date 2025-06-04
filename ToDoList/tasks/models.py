7e665fd27085a4513cb513cb9a5a06bf4d7c4543
r4fael1472 <rafafajorge@gmail.com>
2025-06-03 22:40:43 -0300

    Adicionando algumas funções para contagem e separação de tarefas

--- ToDoList/tasks/models.py
+++ ToDoList/tasks/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User  #usuário padrão do django
+from django.utils import timezone
 
 LISTA_CATEGORIAS = (
     ("PESSOAL", "Pessoal"),
@@ -12,7 +13,7 @@ class Task(models.Model):
     categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
     completo = models.BooleanField(default=False)
     criado_em = models.DateTimeField(auto_now_add=True)
-    data_vencimento = models.DateTimeField()
+    data_vencimento = models.DateField(default=timezone.now)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
--- ToDoList/tasks/novos_context.py
+++ ToDoList/tasks/novos_context.py
@@ -2,11 +2,10 @@ from .models import Task
 from django.utils import timezone
 
 def lista_tarefas_do_dia(request):
-    today = timezone.now().date()
+    today = timezone.now()
     lista_tarefas = Task.objects.all().filter(
-        user=request.user,
         completo=False,
-        data_vencimento__date=today
+        data_vencimento=today
     )
     total_tarefas = lista_tarefas.count()
     return {"lista_tarefas_do_dia": lista_tarefas, "total_tarefas_do_dia": total_tarefas}
@@ -14,7 +13,6 @@ def lista_tarefas_do_dia(request):
 def lista_tarefas_afazer(request):
     now = timezone.now()
     lista_tarefas = Task.objects.all().filter(
-        user=request.user,
         completo=False,
         data_vencimento__gte=now
     )
@@ -23,7 +21,6 @@ def lista_tarefas_afazer(request):
 
 def lista_tarefas_concluidas(request):
     lista_tarefas = Task.objects.all().filter(
-        user=request.user,
         completo=True,
     )
     total_tarefas = lista_tarefas.count()
@@ -32,14 +29,13 @@ def lista_tarefas_concluidas(request):
 def lista_tarefas_atrasadas(request):
     now = timezone.now()
     lista_tarefas = Task.objects.all().filter(
-        user=request.user,
         data_vencimento__lt=now
     ).order_by('data_vencimento')
     total_tarefas = lista_tarefas.count()
     return {"lista_tarefas_atrasadas": lista_tarefas, "total_tarefas_atrasadas": total_tarefas}
 
 def total_tarefas(request):
-    total_tarefas_pessoal = Task.objects.all().filter(user=request.user, categoria='PESSOAL')
-    total_tarefas_estudo = Task.objects.all().filter(user=request.user, categoria='ESTUDOS')
-    total_tarefas_trabalho = Task.objects.all().filter(user=request.user, categoria='TRABALHO')
+    total_tarefas_pessoal = Task.objects.all().filter(categoria='PESSOAL')
+    total_tarefas_estudo = Task.objects.all().filter(categoria='ESTUDOS')
+    total_tarefas_trabalho = Task.objects.all().filter(categoria='TRABALHO')
     return {"total_tarefas_pessoal": total_tarefas_pessoal.count(), "total_tarefas_estudos": total_tarefas_estudo.count(), "total_tarefas_trabalho": total_tarefas_trabalho.count()}

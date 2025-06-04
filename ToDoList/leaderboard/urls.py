17af0da9f21cbc49491f220600b89bae0904807c
Pedro Collares <Pedro.Collares.Xavier@gmail.com>
2025-06-04 18:30:44 -0300

    Issue 22 - Dados mockados do leaderboard e links na barra de navegação lateral agora funcionam

--- ToDoList/leaderboard/urls.py
+++ ToDoList/leaderboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path, reverse_lazy
 from django.contrib.auth import views as auth_view
 from .views import Homepage
+from . import views
 
 
 # url - view - template
@@ -9,4 +10,5 @@ app_name = 'leaderboard'
 urlpatterns = [
     path('tasks/', include('tasks.urls')),
     path('homepage/', Homepage.as_view(), name='homepage'),
+    path('ranking/', views.ranking_view, name='ranking'),
 ]

fce243c08dc08a6442996142cc4b1e472f1170e3
João Pedro Evangelista de Carvalho <joaopedroevangelista@ufrrj.br>
2025-06-24 12:30:59 -0300

    Integração do design da criação de ligas
    Criação do design das páginas de criação de ligas, como a página de inserir um código de convite, além das páginas de ver todas as suas ligas, ver detalhes da sua liga e criar uma liga. Além da atualização das bibliotecas do Python para a versão 3.12, também atualizei as páginas que se relacionam com as páginas de criar ligas.
    
    Co-Authored-By: Lucas Alfeu <103508524+LucasAlfeu@users.noreply.github.com>

--- ToDoList/leaderboard/urls.py
+++ ToDoList/leaderboard/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('calendario/', views.calendario_view, name='calendario'),
     path('get-day-tasks/', views.get_day_tasks, name='get_day_tasks'),
     path('criar-liga/', views.criar_liga, name='criar_liga'),
-    path('ligas/', views.minhas_ligas, name='minhas_ligas'),
-    path('ligas/<int:liga_id>/', views.detalhe_liga, name='detalhe_liga'),
+    path('criar-liga/ligas/', views.minhas_ligas, name='minhas_ligas'),
+    path('criar-liga/ligas/<int:liga_id>/', views.detalhe_liga, name='detalhe_liga'),
     path('ligas/entrar/', views.ingressar_liga, name='ingressar_liga'),
 ]
